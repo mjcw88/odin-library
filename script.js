856326abd9f9bfc64c2a4f9ac8fc17b1bffd87b7
mjcw88 <144539369+mjcw88@users.noreply.github.com>
2026-02-19 19:59:40 +0000

    finished html and css

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() { 
-    
+    const addBookBtn = document.getElementById("add-book");
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const pages = document.getElementById("pages");
+    const read = document.getElementById("read");
+
+    addBookBtn.addEventListener("click", () => {
+        console.log(title.textContent);
+        console.log(author);
+        console.log(pages);
+        console.log(read);
+    })
 });
